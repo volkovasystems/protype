5b4ea5ff080af9ec3ba1783ffe488fa0c0397d0e
volkovasystems <richeve.bebedor@gmail.com>
2017-01-06 09:31:01 +0800

    Added support for multiple type matching.
    Improved codes.

--- protype.js
+++ protype.js
@@ -5,7 +5,7 @@
 		The MIT License (MIT)
 		@mit-license
 
-		Copyright (@c) 2016 Richeve Siodina Bebedor
+		Copyright (@c) 2017 Richeve Siodina Bebedor
 		@email: richeve.bebedor@gmail.com
 
 		Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -46,6 +46,8 @@
 
 	@module-documentation:
 		Determine type of property.
+
+		Passing multiple type will do try to match any one of it.
 	@end-module-documentation
 
 	@include:
@@ -58,6 +60,46 @@
 
 const cemento = require( "cemento" );
 const harden = require( "harden" );
+
+//: @support-module:
+	//: @reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+	Array.isArray||(Array.isArray=function(r){return"[object Array]"===Object.prototype.toString.call(r)});
+//: @end-support-module
+
+//: @support-module:
+	//: @reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+	Array.from||(Array.from=function(){var r=Object.prototype.toString,n=function(n){
+	return"function"==typeof n||"[object Function]"===r.call(n)},t=function(r){var n=Number(r);
+	return isNaN(n)?0:0!==n&&isFinite(n)?(n>0?1:-1)*Math.floor(Math.abs(n)):n},
+	e=Math.pow(2,53)-1,o=function(r){var n=t(r);return Math.min(Math.max(n,0),e)};
+	return function(r){var t=this,e=Object(r);
+	if(null==r)throw new TypeError("Array.from requires an array-like object - not null or undefined");
+	var a,u=arguments.length>1?arguments[1]:void 0;if("undefined"!=typeof u){
+	if(!n(u))throw new TypeError("Array.from: when provided, the second argument must be a function");
+	arguments.length>2&&(a=arguments[2])}for(var i,f=o(e.length),c=n(t)?
+	Object(new t(f)):new Array(f),h=0;f>h;)i=e[h],
+	u?c[h]="undefined"==typeof a?u(i,h):u.call(a,i,h):c[h]=i,h+=1;return c.length=f,c}}());
+//: @end-support-module
+
+//; @support-module:
+	//: @reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+	Array.prototype.some=Array.prototype.some||function(evaluator,thisArg){"use strict";
+	if(!this)throw new TypeError("Array.prototype.some called on null or undefined");
+	if("function"!=typeof evaluator){if("string"!=typeof evaluator)throw new TypeError;
+	if(!(evaluator=eval(evaluator)))throw new TypeError}var i;
+	if(void 0===thisArg){for(i in this)if(evaluator(this[i],i,this))return!0;return!1}
+	for(i in this)if(evaluator.call(thisArg,this[i],i,this))return!0;return!1};
+//; @end-support-module
+
+//: @support-module:
+	//: @reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+	Array.prototype.reduce||(Array.prototype.reduce=function(r){"use strict";
+	if(null==this)throw new TypeError("Array.prototype.reduce called on null or undefined");
+	if("function"!=typeof r)throw new TypeError(r+" is not a function");
+	var e,t=Object(this),n=t.length>>>0,o=0;if(2==arguments.length)e=arguments[1];
+	else{for(;n>o&&!(o in t);)o++;if(o>=n)throw new TypeError("Reduce of empty array with no initial value");
+	e=t[o++]}for(;n>o;o++)o in t&&(e=r(e,t[o],o,t));return e});
+//: @end-support-module
 
 harden( "STRING", "string" );
 harden( "NUMBER", "number" );
@@ -73,13 +115,16 @@ const protype = function protype( property, type ){
 			{
 				"property:required": "*",
 				"type": [
+					"string",
 					STRING,
 					NUMBER,
 					BOOLEAN,
 					FUNCTION,
 					OBJECT,
 					UNDEFINED,
-					SYMBOL
+					SYMBOL,
+					"...",
+					Array
 				]
 			}
 		@end-meta-configuration
@@ -99,7 +144,31 @@ const protype = function protype( property, type ){
 	}
 
 	if( type ){
-		return ( typeof property == type );
+		let flatten = function flatten( current ){
+			return current.reduce( ( previous, current ) => {
+				let element = Array.isArray( current )? flatten( current ) : current;
+
+				return previous.concat( element );
+			}, [ ] );
+		};
+
+		return Array.from( arguments ).splice( 1 )
+			.reduce( ( previous, current ) => {
+				let element = Array.isArray( current )? flatten( current ) : current;
+
+				return previous.concat( element );
+			}, [ ] )
+			.filter( ( type ) => { return ( typeof type == STRING ); } )
+			.filter( ( type ) => {
+				return ( type == STRING ||
+					type == NUMBER ||
+					type == BOOLEAN ||
+					type == FUNCTION ||
+					type == OBJECT ||
+					type == UNDEFINED ||
+					type == SYMBOL );
+			} )
+			.some( ( type ) => { return ( typeof property == type ); } );
 
 	}else{
 		return cemento( {
