9036b75bb7f75787001d5ada57c3d5728d063d88
johnlenon <johnlenonmaghanoy@gmail.com>
2017-02-07 12:43:18 +0800

    Updated test.

--- protype-test.js
+++ protype-test.js
@@ -10,3 +10,5 @@ console.log( protype( true, STRING + NUMBER + OBJECT ) );
 console.log( protype( true, STRING + NUMBER + BOOLEAN ) );
 
 console.log( require( "util" ).inspect( protype( { } ), { "showHidden": true } ) );
+
+console.log( require( "util" ).inspect( protype( ), { "showHidden": true } ) );
