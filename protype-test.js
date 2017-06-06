3b93b0c62ff1b79fe4eb7a9406d60d3048861db9
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-06 15:46:26 +0800

    Updated test.

--- protype-test.js
+++ protype-test.js
@@ -1,14 +1,30 @@
-"use strict";
-
+const assert = require( "assert" );
 const protype = require( "./protype.js" );
 
-console.log( protype( "hello", STRING ) );
+let test1 = protype( );
+assert.equal( test1[ "UNDEFINED" ], true, "should be true" );
+assert.equal( protype( undefined, UNDEFINED ), true, "should be true" );
+assert.equal( protype( 0, NUMBER ), true, "should be true" );
+assert.equal( protype( NaN, NUMBER ), true, "should be true" );
 
-console.log( protype( false, STRING, "yeah", BOOLEAN ) );
+assert.equal( protype( "", STRING ), true, "should be true" );
+assert.equal( protype( "", NUMBER + STRING ), true, "should be true" );
+assert.equal( protype( "hello", STRING ), true, "should be true" );
+assert.equal( protype( false, STRING, "yeah", BOOLEAN ), true, "should be true" );
+assert.equal( protype( true, STRING + NUMBER + BOOLEAN ), true, "should be true" );
+assert.equal( protype( 123, NUMBER ), true, "should be true" );
+assert.equal( protype( function hello( ){ }, FUNCTION ), true, "should be true" );
+assert.equal( protype( Symbol.for( "hello" ), SYMBOL ), true, "should be true" );
 
-console.log( protype( true, STRING + NUMBER + OBJECT ) );
-console.log( protype( true, STRING + NUMBER + BOOLEAN ) );
+assert.equal( protype( null, OBJECT ), true, "should be true" );
+let test2 = protype( { } );
+assert.equal( test2[ "OBJECT" ], true, "should be true" );
+assert.equal( protype( [ ], OBJECT ), true, "should be true" );
+assert.equal( protype( [ Object ], OBJECT ), true, "should be true" );
+assert.equal( protype( new Date( ), OBJECT ), true, "should be true" );
 
-console.log( require( "util" ).inspect( protype( { } ), { "showHidden": true } ) );
+assert.equal( protype( null, UNDEFINED ), false, "should be false" );
+assert.equal( protype( true, STRING + NUMBER + OBJECT ), false, "should be false" );
+assert.equal( protype( 123 + "a", NUMBER ), false, "should be false" );
 
-console.log( require( "util" ).inspect( protype( ), { "showHidden": true } ) );
+console.log( "ok" );
